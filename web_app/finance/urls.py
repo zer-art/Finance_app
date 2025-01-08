957f4b9c625769df06a9b491879b7a25ed4111f1
zer-art <135092407+zer-art@users.noreply.github.com>
2025-01-08 16:18:30 +0530

    fundamental temlates creation
    fundamental templates added

--- web_app/finance/urls.py
+++ web_app/finance/urls.py
@@ -5,4 +5,8 @@ from finance import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("investment/", views.investment, name="investment"),
+    path("loan/", views.loan, name="loan"),  # Added loan URL
+    path("editprofile/", views.editprofile, name="editprofile"),  # Added 
+    path("preferences/", views.preferences, name="preferences"),  # Added preferences URL
+    path("security/", views.security, name="security"),  # Added security URL
 ]
--- web_app/finance/views.py
+++ web_app/finance/views.py
@@ -7,3 +7,16 @@ def home(request):
 
 def investment(request):
     return render(request, "investment.html")
+
+def loan(request):
+    return render(request, "loan.html")
+
+def editprofile(request):
+    return render(request, "editprofile.html")
+
+def preferences(request):
+    return render(request, "preferences.html")  # Added preferences view
+
+def security(request):
+    return render(request, "security.html")  # Added security view
+
